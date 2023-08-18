cb5cdee150358e6381d851882e6dbadc8aadf590
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-08-18 21:10:14 +0430

    Fix Search Controller For User Manager

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -26,6 +26,28 @@ class SearchController extends Controller
                 $search->orderBy('name','asc');
                 $result = $search->get();
                 return response()->json($result);
+                break;
+            case 'UserManagerSearch':
+                $username = $request->input('username');
+                $type = $request->input('type');
+                $search = User::query();
+                if ($username && $type) {
+                    $search->where('username', 'LIKE', '%' . $username . '%')
+                        ->where(function ($query) use ($type) {
+                            $query->where('type', $type);
+                        });
+                    $this->logActivity('Search In User Manager With Username => ' . $username . ' And Type => ' . $type, request()->ip(), request()->userAgent(), session('id'));
+                } elseif ($username) {
+                    $search->where('username', 'LIKE', '%' . $username . '%');
+                    $this->logActivity('Search In User Manager With Username => ' . $username, request()->ip(), request()->userAgent(), session('id'));
+                } elseif ($type) {
+                    $search->where(function ($query) use ($type) {
+                        $query->where('type', $type);
+                    });
+                    $this->logActivity('Search In User Manager With Type => ' . $type, request()->ip(), request()->userAgent(), session('id'));
+                }
+                $result = $search->get();
+                return response()->json($result);
         }
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -207,14 +207,14 @@ $(document).ready(function () {
                                 case 1:
                                     type = 'ادمین کل';
                                     break;
-                                case 5:
-                                    type = 'سازمان';
+                                case 2:
+                                    type = 'کارشناس ستاد';
                                     break;
-                                case 6:
-                                    type = 'باسکول';
+                                case 3:
+                                    type = 'کارشناس فناوری استان';
                                     break;
                             }
-                            var row = '<tr class="bg-white"><td class="px-6 py-4">' + user.id + '</td><td class="px-6 py-4">' + user.name + '</td><td class="px-6 py-4">' + type + '</td>';
+                            var row = '<tr class="bg-white"><td class="px-6 py-4">' + user.username + '</td><td class="px-6 py-4">' + user.name + '</td><td class="px-6 py-4">' + type + '</td>';
                             if (user.active == 1) {
                                 row += '<td class="px-6 py-4">' + '<button type="submit" data-username="' + user.username + '" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-blue-300 ASUM" data-active="1">غیرفعال‌سازی</button>';
                             } else if (user.active == 0) {
@@ -259,14 +259,14 @@ $(document).ready(function () {
                                 case 1:
                                     type = 'ادمین کل';
                                     break;
-                                case 5:
-                                    type = 'سازمان';
+                                case 2:
+                                    type = 'کارشناس ستاد';
                                     break;
-                                case 6:
-                                    type = 'باسکول';
+                                case 3:
+                                    type = 'کارشناس فناوری استان';
                                     break;
                             }
-                            var row = '<tr class="bg-white"><td class="px-6 py-4">' + user.id + '</td><td class="px-6 py-4">' + user.name + '</td><td class="px-6 py-4">' + type + '</td>';
+                            var row = '<tr class="bg-white"><td class="px-6 py-4">' + user.username + '</td><td class="px-6 py-4">' + user.name + '</td><td class="px-6 py-4">' + type + '</td>';
                             if (user.active == 1) {
                                 row += '<td class="px-6 py-4">' + '<button type="submit" data-username="' + user.username + '" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-blue-300 ASUM" data-active="1">غیرفعال‌سازی</button>';
                             } else if (user.active == 0) {
