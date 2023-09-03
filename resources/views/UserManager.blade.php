4d56e7ada5121f05dbcd9746c789965d7a6ae9da
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-09-03 17:21:39 +0430

    Remove Select2 From UserManager

--- resources/views/UserManager.blade.php
+++ resources/views/UserManager.blade.php
@@ -135,7 +135,7 @@
                                                        class="w-60 mt-3 text-gray-700 text-sm font-bold mb-2">کاربر
                                                     را انتخاب کنید</label>
                                                 <select id="userIdForEdit" name="userIdForEdit"
-                                                        class=" w-full inline-block bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline select2">
+                                                        class=" w-full inline-block bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                                                     <option value="" selected disabled>انتخاب کنید</option>
                                                     @foreach($userList as $user)
                                                         <option
