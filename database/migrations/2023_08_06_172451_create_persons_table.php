7a08572bb756acff4d22577ba7206904840d53f2
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-09-27 23:28:14 +0330

    Person Values Added To Migration

--- database/migrations/2023_08_06_172451_create_persons_table.php
+++ database/migrations/2023_08_06_172451_create_persons_table.php
@@ -30,16 +30,18 @@ return new class extends Migration
             $table->unsignedBigInteger('establishment_place')->nullable();
             $table->foreign('establishment_place')->references('id')->on('establishment_places');
             $table->string('room_number')->nullable();
-            $table->unsignedBigInteger('adder');
+            $table->unsignedBigInteger('adder')->default(1);
             $table->foreign('adder')->references('id')->on('users');
             $table->unsignedBigInteger('editor')->nullable();
             $table->foreign('editor')->references('id')->on('users');
             $table->timestamps();
             $table->softDeletes();
         });
-        $query="INSERT INTO persons (id, name, family, personnel_code, national_code, phone, mobile, net_username, work_place, assistance, establishment_place, room_number,adder, created_at, updated_at, deleted_at) VALUES
-(1, 'علی', 'حسابی', NULL, '1234567891', NULL, NULL, NULL, 35, 16, 10, '452',1, '2023-08-25 15:04:45', '2023-08-25 15:04:45', NULL);           ";
-        DB::statement($query);
+//        $query="INSERT INTO persons (id, name, family, personnel_code, national_code, phone, mobile, net_username, work_place, assistance, establishment_place, room_number,adder, created_at, updated_at, deleted_at) VALUES
+//(1, 'علی', 'حسابی', NULL, '1234567891', NULL, NULL, NULL, 35, 16, 10, '452',1, '2023-08-25 15:04:45', '2023-08-25 15:04:45', NULL);           ";
+//        DB::statement($query);
+        DB::unprepared(file_get_contents('database/migrations/persons.sql'));
+
     }
 
     /**
