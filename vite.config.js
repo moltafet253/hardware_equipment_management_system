5c48566c2fb4fedde622c2646dd61b81228e0e61
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-10-11 12:55:15 +0330

    Set inputs in local folder

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/login.css','resources/js/login.js','resources/css/app.css', 'resources/js/app.js'],
+            input: ['./resources/css/login.css','./resources/js/login.js','./resources/css/app.css', './resources/js/app.js'],
             refresh: true,
         }),
         vue(),
