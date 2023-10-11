9b7e4b8ba59f0fc1e8a8c450bfa51da48871acb6
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-10-11 18:15:29 +0330

    Add basic ssl to vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from 'tailwindcss';
+import basicSSL from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
     plugins: [
         laravel({
@@ -9,6 +10,7 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
+        basicSSL(),
     ],
     css: {
         postcss: {
