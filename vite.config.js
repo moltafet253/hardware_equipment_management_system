9f995e60a0a057dd0ee1854076e172df7f59dec4
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-10-11 12:46:54 +0330

    Reverse Changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from 'tailwindcss';
 export default defineConfig({
-    base: '/hib.ismc.ir/',
-    server: {
-        https: true,
-    },
     plugins: [
         laravel({
             input: ['resources/css/login.css','resources/js/login.js','resources/css/app.css', 'resources/js/app.js'],
