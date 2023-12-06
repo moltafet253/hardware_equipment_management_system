5df7e62ddc457cfcd952c76724a11c3517585077
Mohammad <exploiter0253@gmail.com>
2023-12-06 11:40:01 +0330

    Temporary add response

--- resources/js/app.js
+++ resources/js/app.js
@@ -6093,6 +6093,7 @@ $(document).ready(function () {
                     type: 'POST', url: '/BackupDatabase', headers: {
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
                     }, success: function (response) {
+                        console.log(response);
                         if (response.errors) {
                             if (response.errors.error) {
                                 swalFire('خطا!', response.errors.error[0], 'error', 'تلاش مجدد');
