494f4e42ab2fb387f87859325e7d7413f6b70097
Mohammad <exploiter0253@gmail.com>
2023-12-31 13:03:59 +0330

    Fix showing history bugs

--- resources/views/History/HistoryFromPropertyNumber.blade.php
+++ resources/views/History/HistoryFromPropertyNumber.blade.php
@@ -6,6 +6,18 @@
     use App\Models\Person;
     use App\Models\User;
     use Morilog\Jalali\Jalalian;
+    use App\Models\Catalogs\Company;
+    use App\Models\Catalogs\Cases;
+    use App\Models\Catalogs\Monitor;
+    use App\Models\Catalogs\Motherboard;
+    use App\Models\Catalogs\cpu;
+    use App\Models\Catalogs\Ram;
+    use App\Models\Catalogs\Harddisk;
+    use App\Models\Catalogs\GraphicCard;
+    use App\Models\Catalogs\NetworkCard;
+    use App\Models\Catalogs\Odd;
+    use App\Models\Catalogs\Printer;
+    use App\Models\Catalogs\Scanner;
 @endphp
 
 @section('content')
@@ -56,6 +68,7 @@
                                                 {{ $personInfo->personnel_code . ' - ' . $personInfo->name . ' ' . $personInfo->family }}
                                             </td>
                                             <td class="px-6 py-1">
+
                                                 @php
                                                     if (str_contains($logs->title, 'Added')) {
                                                         echo __('messages.device_added', ['property_number' => $logs->property_number]);
@@ -65,18 +78,115 @@
                                                         $nextLog = EquipmentLog::find(++$logs->id);
                                                         if ($nextLog) {
                                                             foreach (json_decode($nextLog->title, true) as $value) {
-                                                                echo __('messages.changed_property_number', ['from' => $value['from'], 'to' => $value['to']]);
+                                                                switch ($value['field']) {
+                                                                    case 'case_id':
+                                                                    case 'delivery_date':
+                                                                        echo 'تغییر تاریخ تحویل از: ' . $value['from'] . ' به: ' . $value['to'] . '<br/>';
+                                                                        break;
+                                                                    case 'property_number':
+                                                                        echo 'تغییر کد اموال از: ' . $value['from'] . ' به: ' . $value['to'] . '<br/>';
+                                                                        break;
+                                                                    case 'case':
+                                                                        $from_company = Cases::with('company')->find($value['from']);
+                                                                        $to_company = Cases::with('company')->find($value['to']);
+                                                                        echo 'تغییر کیس از: ' . $from_company->company->name . ' ' . $from_company->model . ' به: ' . $to_company->company->name . ' ' . $to_company->model . '<br/>';
+                                                                        break;
+                                                                    case 'motherboard':
+                                                                        $from_company = Motherboard::with('company')->find($value['from']);
+                                                                        $to_company = Motherboard::with('company')->find($value['to']);
+                                                                        echo 'تغییر مادربورد از: ' . $from_company->company->name . ' ' . $from_company->model . ' به: ' . $to_company->company->name . ' ' . $to_company->model . '<br/>';
+                                                                        break;
+                                                                    case 'cpu':
+                                                                        $from_company = cpu::with('company')->find($value['from']);
+                                                                        $to_company = cpu::with('company')->find($value['to']);
+                                                                        echo 'تغییر پردازنده از: ' . $from_company->company->name . ' ' . $from_company->model . ' به: ' . $to_company->company->name . ' ' . $to_company->model . '<br/>';
+                                                                        break;
+                                                                        break;
+                                                                    case 'ram1':
+                                                                        $from_company = Ram::with('company')->find($value['from']);
+                                                                        $to_company = Ram::with('company')->find($value['to']);
+                                                                        echo 'تغییر رم 1 از: ' . $from_company->company->name . ' ' . $from_company->model . ' ' . $from_company->type . ' ' . $from_company->size . ' ' . $from_company->frequerncy . ' به: ' . $to_company->company->name . ' ' . $to_company->model . ' ' . $to_company->type . ' ' . $to_company->size . ' ' . $to_company->frequerncy . '<br/>';
+                                                                        break;
+                                                                    case 'ram2':
+                                                                        $from_company = Ram::with('company')->find($value['from']);
+                                                                        $to_company = Ram::with('company')->find($value['to']);
+                                                                        echo 'تغییر رم 2 از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->type . ' ' . @$from_company->size . ' ' . @$from_company->frequerncy . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->type . ' ' . @$to_company->size . ' ' . @$to_company->frequerncy . '<br/>';
+                                                                        break;
+                                                                    case 'ram3':
+                                                                        $from_company = Ram::with('company')->find($value['from']);
+                                                                        $to_company = Ram::with('company')->find($value['to']);
+                                                                        echo 'تغییر رم 3 از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->type . ' ' . @$from_company->size . ' ' . @$from_company->frequerncy . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->type . ' ' . @$to_company->size . ' ' . @$to_company->frequerncy . '<br/>';
+                                                                        break;
+                                                                    case 'ram4':
+                                                                        $from_company = Ram::with('company')->find($value['from']);
+                                                                        $to_company = Ram::with('company')->find($value['to']);
+                                                                        echo 'تغییر رم 4 از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->type . ' ' . @$from_company->size . ' ' . @$from_company->frequerncy . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->type . ' ' . @$to_company->size . ' ' . @$to_company->frequerncy . '<br/>';
+                                                                        break;
+                                                                    case 'hdd1':
+                                                                        $from_company = Harddisk::with('company')->find($value['from']);
+                                                                        $to_company = Harddisk::with('company')->find($value['to']);
+                                                                        echo 'تغییر هارد 1 از: ' . $from_company->company->name . ' ' . $from_company->model . ' ' . $from_company->capacity . ' ' . $from_company->type . ' ' . $from_company->connectivity_type . ' به: ' . $to_company->company->name . ' ' . $to_company->model . ' ' . $to_company->capacity . ' ' . $to_company->type . ' ' . $to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'hdd2':
+                                                                        $from_company = Harddisk::with('company')->find($value['from']);
+                                                                        $to_company = Harddisk::with('company')->find($value['to']);
+                                                                        echo 'تغییر هارد 2 از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->capacity . ' ' . @$from_company->type . ' ' . @$from_company->connectivity_type . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->capacity . ' ' . @$to_company->type . ' ' . @$to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'hdd3':
+                                                                        $from_company = Harddisk::with('company')->find($value['from']);
+                                                                        $to_company = Harddisk::with('company')->find($value['to']);
+                                                                        echo 'تغییر هارد 3 از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->capacity . ' ' . @$from_company->type . ' ' . @$from_company->connectivity_type . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->capacity . ' ' . @$to_company->type . ' ' . @$to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'hdd4':
+                                                                        $from_company = Harddisk::with('company')->find($value['from']);
+                                                                        $to_company = Harddisk::with('company')->find($value['to']);
+                                                                        echo 'تغییر هارد 4 از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->capacity . ' ' . @$from_company->type . ' ' . @$from_company->connectivity_type . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->capacity . ' ' . @$to_company->type . ' ' . @$to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'graphic_card':
+                                                                        $from_company = GraphicCard::with('company')->find($value['from']);
+                                                                        $to_company = GraphicCard::with('company')->find($value['to']);
+                                                                        echo 'تغییر کارت گرافیک از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->capacity . ' ' . @$from_company->type . ' ' . @$from_company->connectivity_type . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->capacity . ' ' . @$to_company->type . ' ' . @$to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'network_card':
+                                                                        $from_company = NetworkCard::with('company')->find($value['from']);
+                                                                        $to_company = NetworkCard::with('company')->find($value['to']);
+                                                                        echo 'تغییر کارت شبکه از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->capacity . ' ' . @$from_company->type . ' ' . @$from_company->connectivity_type . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->capacity . ' ' . @$to_company->type . ' ' . @$to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'odd':
+                                                                        $from_company = Odd::with('company')->find($value['from']);
+                                                                        $to_company = Odd::with('company')->find($value['to']);
+                                                                        echo 'تغییر کارت شبکه از: ' . @$from_company->company->name . ' ' . @$from_company->model . ' ' . @$from_company->capacity . ' ' . @$from_company->type . ' ' . @$from_company->connectivity_type . ' به: ' . @$to_company->company->name . ' ' . @$to_company->model . ' ' . @$to_company->capacity . ' ' . @$to_company->type . ' ' . @$to_company->connectivity_type . '<br/>';
+                                                                        break;
+                                                                    case 'monitor_id':
+                                                                        $from = Monitor::find($change['from']);
+                                                                        $from_company = Company::find($from->company_id);
+                                                                        $to = Monitor::find($change['to']);
+                                                                        $to_company = Company::find($to->company_id);
+                                                                        echo 'تغییر مانیتور از: ' . $from_company->name . ' ' . $from->model . ' به: ' . $to_company->name . ' ' . $to->model;
+                                                                        break;
+                                                                    case 'printer_id':
+                                                                        $from = Printer::find($change['from']);
+                                                                        $from_company = Company::find($from->company_id);
+                                                                        $to = Printer::find($change['to']);
+                                                                        $to_company = Company::find($to->company_id);
+                                                                        echo 'تغییر پرینتر از: ' . $from_company->name . ' ' . $from->model . ' ' . $from->function_type . ' به: ' . $to_company->name . ' ' . $to->model . ' ' . $to->function_type;
+                                                                        break;
+                                                                    case 'scanner_id':
+                                                                        $from = Scanner::find($change['from']);
+                                                                        $from_company = Company::find($from->company_id);
+                                                                        $to = Scanner::find($change['to']);
+                                                                        $to_company = Company::find($to->company_id);
+                                                                        echo 'تغییر اسکنر از: ' . $from_company->name . ' ' . $from->model . ' ' . $from->function_type . ' به: ' . $to_company->name . ' ' . $to->model . ' ' . $to->function_type;
+                                                                        break;
+                                                                    default:
+                                                                    // اگر نیاز به انجام عملیاتی برای حالت‌های دیگر هست، می‌توانید اینجا اضافه کنید.
+                                                                }
                                                             }
                                                         } else {
                                                             echo 'null';
                                                         }
-                                                    } else {
-                                                @endphp
-                                                @include ('History.Translate.history')
-                                                @php
                                                     }
                                                 @endphp
-
                                             </td>
                                             <td class="px-6 py-1">
                                                 {{ Jalalian::fromDateTime($logs->created_at)->format('H:m Y/m/d') }}
